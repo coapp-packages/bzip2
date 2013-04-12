d5fc47389625d312c9232dc729912448011e425f
Tim Rogers <tiroger@microsoft.com>
2013-04-12 11:12:45 -0700

    added .vcxproj file, work in progress

---
+++ COPKG/all.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal
+
+if "%1"=="clean" goto :clean
+if "%1"=="noclean" (
+	set __NOCLEAN__=true
+	shift)
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v110
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v110
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
+call :build x64 Release v100
+endlocal
+
+setlocal
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+call :build Win32 Release v100
+endlocal
+
+if "__NOCLEAN__"=="true" goto :eof
+
+goto :clean
+
+:build
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=DynamicLibrary /P:CallingConvention=Cdecl .\bzip.vcxproj
+msbuild /P:Platform=%1 /P:Configuration=%2 /P:PlatformToolset=%3 /P:ConfigurationType=StaticLibrary /P:CallingConvention=Cdecl .\bzip.vcxproj
+
+goto :eof
+
+:clean
+REM rd /s /q v90
+rd /s /q v100
+rd /s /q v110
+
